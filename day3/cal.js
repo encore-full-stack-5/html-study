8db94c2876d800210e3ad9366729c5cb9a6fb407
tjdan805 <tjdan805@gmail.com>
2024-03-25 16:03:08 +0900

    gtml\

--- day3/cal.js
+++ day3/cal.js
@@ -24,5 +24,8 @@ cal[2](1);
 cal[3](10);
 cal[4](2);
 cal[1](4);
-const [total] = cal;
-console.log(cal[5], cal[0] === total);
+const [total, c1, c2, c3, c4, c5] = cal;
+console.log(cal[5], cal[0] === total, c1 === cal[1]);
+
+// 수: mongo  vue
+// 목: elk  next
