4950e08dafc4702efe9d3aedb10c4425eba9d8f4
tjdan805 <tjdan805@gmail.com>
2024-03-26 15:02:30 +0900

    scroll

--- day5/ex1.js
+++ day5/ex1.js
@@ -12,9 +12,9 @@ const observer = new IntersectionObserver((entries, observer) => {
   // 처음에 실행이 되고, 타겟들이 보이면 다시 호출된다.
   entries.forEach((entrie) => {
     if (entrie.isIntersecting) {
-      console.log("확인");
+      entrie.target.className = "box show";
     } else {
-      console.log("미확인");
+      entrie.target.className = "box";
     }
   });
 });
