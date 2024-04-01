5f042114ffdebfa7617f3ec2ffc0fecca20b7612
tjdan805 <tjdan805@gmail.com>
2024-04-01 16:30:30 +0900

    test

--- style/src/App.jsx
+++ style/src/App.jsx
@@ -8,11 +8,17 @@ import "./App.css";
 //  git switch master
 //  git switch main
 //  git switch -c bootstrap
+//  git switch -c main
+//  git switch -c tailwind
 function App() {
+  const [col, setCol] = useState("#be2020");
+  console.log(col);
   return (
     <>
       <div className="box"></div>
       <div className="box"></div>
+      <input type="color" onChange={(e) => setCol(e.target.value)} />
+      <div className={`h-11 w-11 bg-[${col}] shadow rounded`}></div>
     </>
   );
 }
