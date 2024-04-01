3b6a6ff0bd46e31dadd825902b5974463b609e40
tjdan805 <tjdan805@gmail.com>
2024-04-01 15:50:20 +0900

    css in js

--- style/src/App.jsx
+++ style/src/App.jsx
@@ -2,17 +2,39 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+
 // git branch
 // git add .
 //  git commit -m "init"
 //  git switch master
 //  git switch main
 //  git switch -c bootstrap
+//  git switch main
+//  git switch -c emotion
+//  npm i @emotion/css
+//  npm i @emotion/react
+import { css } from "@emotion/css";
 function App() {
+  const [color, setColor] = useState("red");
   return (
     <>
       <div className="box"></div>
       <div className="box"></div>
+
+      {/* css in js*/}
+      <div
+        className={css`
+          width: 100px;
+          height: 100px;
+          background-color: ${color};
+          &:hover {
+            background-color: blue;
+            transition: all 2s;
+            transform: rotate(900deg) scale(2);
+          }
+        `}
+      ></div>
+      <input type="color" onChange={(e) => setColor(e.target.value)} />
     </>
   );
 }
