020695534b399025bdd1495cbaf1c179cbdb94c0
tjdan805 <tjdan805@gmail.com>
2024-04-02 16:06:01 +0900

    before route

--- day9/README.md
+++ day9/README.md
@@ -3,10 +3,12 @@
 src/atom --> 가장 단위를 만들 것(button)
 src/components --> components
 src/store --> recoil, redux
-src/pages --> page
+src/pages --> 찐 page
 src/api --> 통신
-src/routes --> route
+src/routes --> page
 src/config --> config
 src/assets --> img
 
 npm install recoil
+
+192.168.80.4
---
+++ day9/src/pages/Signup.jsx
@@ -0,0 +1,24 @@
+import Article from "../atom/Article";
+import Button from "../atom/Button";
+import Input from "../atom/Input";
+import Label from "../atom/Label";
+
+// Signup.jsx
+const Signup = () => {
+  return (
+    <div className="container">
+      <Article>
+        <form>
+          <Label htmlFor="name">name</Label>
+          <Input id="name" required />
+          <Label htmlFor="password">password</Label>
+          <Input id="password" type="password" required />
+          <hr className="m-4" />
+          <Button>submit</Button>
+        </form>
+      </Article>
+    </div>
+  );
+};
+
+export default Signup;
